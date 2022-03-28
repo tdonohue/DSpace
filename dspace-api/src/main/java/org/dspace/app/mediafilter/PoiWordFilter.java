08412eb17f7e87b32542c067d30267d32ca53315
Tim Donohue <tim.donohue@lyrasis.org>
2022-03-28 13:10:30 -0500

    TEST using Tika to parse Word Documents

--- dspace-api/src/main/java/org/dspace/app/mediafilter/PoiWordFilter.java
+++ dspace-api/src/main/java/org/dspace/app/mediafilter/PoiWordFilter.java
@@ -7,13 +7,14 @@
  */
 package org.dspace.app.mediafilter;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.poi.extractor.ExtractorFactory;
-import org.apache.poi.extractor.POITextExtractor;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.ReaderInputStream;
+import org.apache.tika.Tika;
 import org.dspace.content.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,11 +49,10 @@ public class PoiWordFilter
     @Override
     public InputStream getDestinationStream(Item currentItem, InputStream source, boolean verbose)
         throws Exception {
-        String text;
-        try {
-            // get input stream from bitstream, pass to filter, get string back
-            POITextExtractor extractor = ExtractorFactory.createExtractor(source);
-            text = extractor.getText();
+        InputStream textStream;
+        // Use Tika to parse text from input
+        try (Reader reader = new Tika().parse(source)) {
+            textStream = new ReaderInputStream(reader, StandardCharsets.UTF_8);
         } catch (IOException e) {
             System.err.format("Invalid File Format:  %s%n", e.getMessage());
             LOG.error("Unable to parse the bitstream:  ", e);
@@ -61,10 +61,10 @@ public class PoiWordFilter
 
         // if verbose flag is set, print out extracted text to STDOUT
         if (verbose) {
-            System.out.println(text);
+            System.out.println(IOUtils.toString(textStream, StandardCharsets.UTF_8));
         }
 
         // return the extracted text as a stream.
-        return new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+        return textStream;
     }
 }
